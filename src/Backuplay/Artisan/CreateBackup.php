2406b77b73576035a4b9bc3e960ac7efbeb2ed93
Tom Witkowski <dev.gummibeer@gmail.com>
2016-05-04 17:41:50 +0200

    add config add provider add create artisan command

---
+++ src/Backuplay/Artisan/CreateBackup.php
@@ -0,0 +1,16 @@
+<?php
+namespace Gummibeer\Backuplay\Artisan;
+
+use Illuminate\Console\Command;
+
+class CreateBackup extends Command
+{
+
+    protected $name = 'backup:create';
+
+    protected $description = 'Create and store a new backup';
+
+    public function fire()
+    {
+    }
+}
---
+++ src/Backuplay/BackuplayServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+namespace Gummibeer\Backuplay;
+
+use Gummibeer\Backuplay\Artisan\CreateBackup;
+use Illuminate\Support\ServiceProvider;
+
+class BackuplayServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        $this->artisan();
+    }
+
+    public function boot()
+    {
+        $this->config();
+    }
+
+    protected function config()
+    {
+        $this->publishes([
+            __DIR__.'/../config/backuplay.php' => config_path('backuplay.php'),
+        ]);
+
+        $this->mergeConfigFrom(__DIR__.'/../config/backuplay.php', 'backuplay');
+    }
+
+    protected function artisan()
+    {
+        $this->app->singleton('backuplay.artisan.backup-create', function ($app) {
+            return new CreateBackup();
+        });
+
+        $this->commands([
+            'backuplay.artisan.backup-create',
+        ]);
+    }
+}
